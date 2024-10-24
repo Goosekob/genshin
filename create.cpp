391e7a4ea7a0b7e60cfcf2285ef4c17106eedbd7
Anna <glavatskikhas@gmail.com>
2024-10-24 17:18:30 +0300

    update create.cpp

--- create.cpp
+++ create.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main() {
     ofstream outfile("my_file.txt");
-
+    cout << "lorem ipsum";
     if (outfile.is_open()) {
         outfile << "Это текст, который мы записываем в файл.\n"; 
         outfile.close();
